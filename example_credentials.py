684e305c37929d25da82af1a13b293654b0c7df1
LYND$KG <60162813+lyndskg@users.noreply.github.com>
2023-08-05 02:42:59 -0500

    Create example_credentials.py

---
+++ example_credentials.py
@@ -0,0 +1,6 @@
+#Enter Your Credentials Here
+#poshmark_username = "poshmarkusername"
+#poshmark_password = "poshmarkpassword"
+
+#Rename this file "credentials.py"
+#mv example_credentials.py credentials.py
