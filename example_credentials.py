7c1f050e0d9f00552ccb5f6ea755af4d57b5253e
lyndskg <lyndskg@umich.edu>
2023-08-05 04:37:36 -0500

    What

--- example_credentials.py
+++ example_credentials.py
@@ -1,6 +1,11 @@
 #Enter Your Credentials Here
+<<<<<<< HEAD
 #poshmark_username = "poshmarkusername"
 #poshmark_password = "poshmarkpassword"
+=======
+#amazon_email = "myemail@gmail.com"
+#amazon_password = "mysupersecret"
+>>>>>>> 0c255c6 (adding example credentials file)
 
 #Rename this file "credentials.py"
 #mv example_credentials.py credentials.py
